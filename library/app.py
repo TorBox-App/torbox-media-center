67fcaa0451ec1c7db3a110176883d2ae0f673466
Wamy <25761442+Wamy-Dev@users.noreply.github.com>
2025-10-09 03:25:13 -0500

    fix: changes default of mount refresh times, adds 2 new refresh times

--- library/app.py
+++ library/app.py
@@ -6,12 +6,14 @@ load_dotenv()
 
 class MountRefreshTimes(Enum):
     # times are shown in hours
-    slow = 3 # 3 hours
-    normal = 2 # 2 hours
-    fast = 1 # 1 hour
-    instant = 0.10 # 6 minutes
+    slowest = 24 # 24 hours
+    very_slow = 12 # 12 hours
+    slow = 6 # 6 hours
+    normal = 3 # 3 hours
+    fast = 2 # 2 hours
+    instant = 1 # 1 hour
 
-MOUNT_REFRESH_TIME = os.getenv("MOUNT_REFRESH_TIME", MountRefreshTimes.fast.name)
+MOUNT_REFRESH_TIME = os.getenv("MOUNT_REFRESH_TIME", MountRefreshTimes.normal.name)
 MOUNT_REFRESH_TIME = MOUNT_REFRESH_TIME.lower()
 assert MOUNT_REFRESH_TIME in [e.name for e in MountRefreshTimes], f"Invalid mount refresh time: {MOUNT_REFRESH_TIME}. Valid options are: {[e.name for e in MountRefreshTimes]}"
 
